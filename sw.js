9fc39a07f63f93c5a8238b3f263841e654848e23
d20reyes <diegoreyes2009@gmail.com>
2025-05-20 08:39:57 -0700

    push

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then(cache =>
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+      ])
+    )
+  );
+});
